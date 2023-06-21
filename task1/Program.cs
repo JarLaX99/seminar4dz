e1dd393e08feec0f8233c7773d5c7efc122835ca
JarLaX99 <vanek10699@gmail.com>
2023-06-21 19:57:34 +0300

    семинар 4, задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл,который принимает на вход 
+//два числа A и B и возводит A в натуральную степень B
+
+int Readint(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int Pow(int num,int numberN)
+{  
+    int  sum = num;
+
+    for(int i = 1;i<numberN;i++)
+    {
+         sum = sum * num;
+    }
+    return sum;
+}
+
+int numberA = Readint("Введите первое число: ");
+int numberB = Readint("Введите второе число: ");
+
+int result = Pow(numberA,numberB);
+System.Console.WriteLine($"{numberA}^{numberB} = {result}");
