0c4125587ed46e63c0057c7741052dc166f5fd3c
JarLaX99 <vanek10699@gmail.com>
2023-06-21 23:28:29 +0300

    семинар 4, задача 3

--- task3/Program.cs
+++ task3/Program.cs
@@ -6,25 +6,25 @@ int Readint(string text)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-void Fullarray (int[] arr,int leftRange,int rightRange)
+void Fullarray(int[] arr, int leftRange, int rightRange)
 {
-    Random rand = new Random() ;
+    Random rand = new Random();
 
-    for(int i = 0;i<arr.Length;i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        arr[i]= rand.Next(leftRange, rightRange);
+        arr[i] = rand.Next(leftRange, rightRange);
     }
 }
 void PrintArray(int[] arr)
 {
-   System.Console.Write("[");
-   System.Console.Write(string.Join("] [",arr));
-   System.Console.WriteLine("]");
+    System.Console.Write("[");
+    System.Console.Write(string.Join("] [", arr));
+    System.Console.WriteLine("]");
 }
 
 int sizeArray = Readint("Введите размер массива: ");
 int[] array = new int[sizeArray];
 int leftRangeArray = Readint("Введите диапозон генерируемых чисел от: ");
 int rightRangeArray = Readint("Введите диапозон генерируемых чисел до: ");
-Fullarray(array,leftRangeArray,rightRangeArray);
+Fullarray(array, leftRangeArray, rightRangeArray);
 PrintArray(array);
